e8447bef13390a614c0015bdce875213d89f3f0b
Ryan Hemmila <ryanhemmila@gmail.com>
2021-09-29 02:27:49 -0500

    updates

--- HW3/pca.py
+++ HW3/pca.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
 from scipy.linalg import eigh
 import numpy as np
-import matplotlib.pyplot as plt
 
 def load_and_center_dataset(filename):
   x = np.load(filename)
@@ -26,22 +26,55 @@ def get_eig_perc(S, perc):
   vals, vecs = eigh(S)
   val_sum = np.sum(vals)
   filt_vals = []
-  filt_ind = []
+
+  # Filter values that meet threshold
   for i, val in enumerate(vals):
     if (val / val_sum) > perc:
-      filt_vals.append(val)
-      filt_ind.append(i)
-  return np.diag(np.flip(np.array(filt_vals))), np.flip(vecs[:,filt_ind], axis=1)
+      filt_vals.append((val, i))
+
+  # Sort in desc order
+  filt_vals.sort(key = lambda x: x[0], reverse=True)
+
+  # Sort columns
+  indices = [filt_val[1] for filt_val in filt_vals]
+  vecs = vecs[:,indices]
+
+  # Create diagonal matrix
+  arr = np.array([filt_val[0] for filt_val in filt_vals])
+  diag = np.diag(arr)
+
+  return diag, vecs
 
 def project_image(img, U):
-  print("")
-
+  projection = None
+  for j in range(0, U.shape[1]):
+    au = np.dot(np.dot(U[:,j], img), U[:,j])
+    if projection is None:
+      projection = au
+    else:
+      projection += au
+  return projection
 
 def display_image(orig, proj):
-  print("")
 
-x = load_and_center_dataset('YaleB_32x32.npy')
-S = get_covariance(x)
-Lambda, U = get_eig_perc(S, 0.07)
-print(Lambda)
-print(U)
+  # Resize images
+  orig_res = orig.reshape(32, 32)
+  proj_res = proj.reshape(32, 32)
+
+  # Create plots
+  fig, (orig_ax, proj_ax) = plt.subplots(1, 2)
+
+  # Set titles
+  orig_ax.set_title("Original")
+  proj_ax.set_title("Projection")
+  
+  # Display images
+  orig_im = orig_ax.imshow(orig_res.T, aspect='equal')
+  proj_im = proj_ax.imshow(proj_res.T, aspect='equal')
+
+  # Add colorbars
+  fig.colorbar(orig_im, ax=orig_ax)
+  fig.colorbar(proj_im, ax=proj_ax)
+  
+  # Show
+  plt.show()
