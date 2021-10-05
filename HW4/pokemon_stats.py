7b51b579a207248263e714820b3cad62a2df6f62
Ryan Hemmila <ryanhemmila@gmail.com>
2021-10-05 13:53:40 -0500

    progress

--- HW4/pokemon_stats.py
+++ HW4/pokemon_stats.py
@@ -1,3 +1,4 @@
+import numpy as np
 import csv
 
 def load_data(filepath):
@@ -21,3 +22,36 @@ def load_data(filepath):
       })
   return pokemon
 
+def calculate_x_y(stats):
+  x = stats['Attack'] + stats['Sp. Atk'] + stats['Speed']
+  y = stats['Defense'] + stats['Sp. Def'] + stats['HP']
+  return x, y
+
+def hac(dataset):
+  clusters = []
+  for i, data_point in enumerate(dataset):
+    clusters.append(([data_point], i))
+  Z = np.empty((len(dataset), 4))
+  for i in range(len(dataset) - 1):
+    min_clust_dist = float('inf')
+    min_clust_a = None
+    min_clust_b = None
+    for j in range(len(clusters)):
+      for k in range(len(clusters)):
+        if j != k:
+          clust_dist = float('inf')
+          clust_a = clusters[j]
+          clust_b = clusters[k]
+          for data_point_a in clust_a:
+            for data_point_b in clust_b:
+              dist = math.sqrt(abs(data_point_a[0] - data_point_b[0])**2 + abs(data_point_a[1] - data_point_b[1])**2)
+              if dist < clust_dist:
+                clust_dist = dist
+          if clust_dist < min_clust_dist:
+            min_clust_dist = clust_dist
+            min_clust_a = clust_a
+            min_clust_b = clust_b
+
+
+pokemon = load_data("./Pokemon.csv")
+print(calculate_x_y(pokemon[1]))
