7bf119b20ce7e3eab266404e416a394b1bb3728a
Ryan Hemmila <ryanhemmila@gmail.com>
2021-12-14 07:48:39 -0600

    q learning done

--- HW10/Q_learning.py
+++ HW10/Q_learning.py
@@ -23,19 +23,38 @@ if __name__ == "__main__":
   env.seed(1)
   env.action_space.np_random.seed(1)
 
-  Q_table = defaultdict(default_Q_value) # starts with a pessimistic estimate of zero reward for each state.
+  Q_table = defaultdict(default_Q_value)
   episode_reward_record = deque(maxlen=100)
 
   for i in range(EPISODES):
     episode_reward = 0
+    state = env.reset()
+    done = False
+    while not done:
+      action = None
+      if random.uniform(0, 1) < EPSILON:
+        action = env.action_space.sample()
+      else:
+        predictions = np.array([Q_table[(state, j)] for j in range(env.action_space.n)])
+        action = np.argmax(predictions)
 
-    #TODO PERFORM Q LEARNING
+      next_state, reward, done, _ = env.step(action)
 
-    if i % 100 ==0 and i > 0:
+      next_predictions = np.array([Q_table[(next_state, j)] for j in range(env.action_space.n)])
+      target = reward + DISCOUNT_FACTOR * np.max(next_predictions)
+
+      Q_table[(state, action)] = Q_table[(state, action)] + LEARNING_RATE * (target - Q_table[(state, action)])
+      episode_reward += reward
+      state = next_state
+
+    EPSILON *= EPSILON_DECAY
+    episode_reward_record.append(episode_reward)
+
+    if i % 100 == 0 and i > 0:
       print("LAST 100 EPISODE AVERAGE REWARD: " + str(sum(list(episode_reward_record)) / 100))
       print("EPSILON: " + str(EPSILON))
 
-  ####DO NOT MODIFY######
+  #### DO NOT MODIFY ####
   model_file = open('Q_TABLE.pkl' ,'wb')
-  pickle.dump([Q_table,EPSILON],model_file)
+  pickle.dump([Q_table, EPSILON], model_file)
   #######################
